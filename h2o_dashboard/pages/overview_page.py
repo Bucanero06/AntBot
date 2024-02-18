ef613bb86dfe73f8f1998d4d481cf7e41b45ef5d
ruben <ruben@moonshot.codes>
2024-02-18 17:15:57 -0500

    added _ping_services and redis insight to ui

--- h2o_dashboard/pages/overview_page.py
+++ h2o_dashboard/pages/overview_page.py
@@ -61,9 +61,23 @@ async def add_tradingview_advanced_chart(q: Q, card_name: str, box: str):
 
 
 # Usage
+async def _ping_services(q):
+    try:
+        header_card = q.page['Overview_Page_Header']
+        rest_handling_check, websocket_handling_check = await asyncio.gather(
+            q.run(requests.request, 'GET', 'http://localhost:8080/health'),
+            q.run(requests.request, 'GET', 'http://localhost:8081/health'),
+        )
+
+        header_card.subtitle = f'Rest Service Ping: {rest_handling_check.text}\n' \
+                               f'Websockets Service Ping: {websocket_handling_check.text}\n '
+
+    except Exception as e:
+        header_card.subtitle = f'Error: {e}'
+        print(f'{e = }')
 
 
-async def overview_page(q: Q):
+async def overview_page(q: Q, update_seconds: int = 2):
     print("Loading Overview Page")
     '''Header'''
     await add_card(q, 'Overview_Page_Header',
@@ -79,9 +93,15 @@ async def overview_page(q: Q):
                                       ),
                                   ]
                                   ))
-    header_card = q.page['Overview_Page_Header']
 
     await add_tradingview_advanced_chart(q, card_name='Overview_Page_TradingView_Advanced_Chart', box='grid_1')
+
+    # Redis Iframe Card
+    await add_card(q, 'Redis_Iframe_Card',
+                   ui.form_card(box='grid_3', items=[
+                       ui.frame(content="""<iframe src="http://localhost:8001" width="100%" height="100%"></iframe>""",
+                                height="800px", width="100%")
+                   ]))
 
     '''Init Widgets'''
     overview_widget = Overview_StreamWidget(q=q, card_name='grid_2', box='grid_2', count=2)
@@ -98,7 +118,7 @@ async def overview_page(q: Q):
             if not q.client.overview_page_running_event.is_set():
                 print("Breaking Overview Page Loop")
                 break
-            await asyncio.sleep(1)
+            await asyncio.sleep(update_seconds)
 
             '''CARD UPDATES'''
             if await overview_widget._is_initialized():
@@ -108,19 +128,11 @@ async def overview_page(q: Q):
                 print("Adding Overview card")
                 await overview_widget.add_cards()
 
-            '''Ping Services and Update UI Component'''
-            try:
+            await _ping_services(q)
 
-                rest_handling_check, websocket_handling_check = await asyncio.gather(
-                    q.run(requests.request, 'GET', 'http://localhost:8080/health'),
-                    q.run(requests.request, 'GET', 'http://localhost:8081/health'),
-                )
 
-                header_card.subtitle = f'Rest Service Ping: {rest_handling_check.text}\n' \
-                                       f'Websockets Service Ping: {websocket_handling_check.text}\n '
 
-            except Exception as e:
-                print(f'{e = }')
+
             await q.page.save()
     except asyncio.CancelledError:
         print("Cancelled")
